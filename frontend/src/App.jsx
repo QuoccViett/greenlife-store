d7bedf68ef8ff4c86f469482ff603c3359312f14
viet <3123560093@sv.sgu.edu.vn>
2026-03-25 23:34:48 +0700

    HomePage

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,13 @@
 import './App.css'
 import Footer from './components/Footer'
 import Navbar from './components/Navbar'
+import HomePage from './pages/HomePage'
 
 function App() {
   return (
     <div className='min-h-screen flex flex-col'>
       <Navbar />
-
+      <HomePage/>
       <Footer/>
     </div>
   )
