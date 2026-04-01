af7de507b35761a058d2c99e3ddcd6ca2c84070e
viet <3123560093@sv.sgu.edu.vn>
2026-04-01 14:10:54 +0700

    Update AdminProduct

--- backend/controllers/categoryController.js
+++ backend/controllers/categoryController.js
@@ -4,7 +4,8 @@ const Category = require('../models/Category')
 
 const getCategories = async (req, res) => {
     try {
-        const categories = await Category.find()
+        const categories = await Category
+            .find({ parent: null })
         res.json(categories)
     } catch (error) {
         res.status(500).json({message: error.message})
