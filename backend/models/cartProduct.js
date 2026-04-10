b93f2c829ee84e0d9ec996ebe49cf125411cbb79
HuyPNT <your-email@example.com>
2026-04-10 21:47:28 +0700

    update cart feature

---
+++ backend/models/cartProduct.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+
+const addToCart = mongoose.Schema(
+  {
+    productId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Product",
+      required: true,
+    },
+    quantity: {
+      type: Number,
+      required: true,
+      min: 1,
+    },
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+const addToCartModel = mongoose.model("addToCart", addToCart);
+
+module.exports = addToCartModel;
